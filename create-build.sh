10d7712689369486d8fee8e78b552c2bf6c45ffe
Devang Ray <gbc2rx@virginia.edu>
2024-09-17 23:44:11 -0400

    renamed script.sh, and made script to create build folder

---
+++ create-build.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+echo "This is creating a build folder. Confirm that you need to make a build folder"
+read -p "Continue? (Y/N): " confirm && [[ $confirm == [yY] || $confirm == [yY][eE][sS] ]] || exit 1
+echo "creating build folder"
+. ~/.bashrc
+mkdir build
+cd build
+echo "running cmake"
+cmake ..
+echo "running make"
+make
+echo "ran"
