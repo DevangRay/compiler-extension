6727f440f415955eba3f0490ba3eda409a47513b
Devang Ray <gbc2rx@virginia.edu>
2024-09-23 12:08:55 -0400

    added positive and negative cases for <=

--- test/unit/frontend/SIPParserTest.cpp
+++ test/unit/frontend/SIPParserTest.cpp
@@ -5,7 +5,7 @@
 
 #include <catch2/catch_test_macros.hpp>
 
-TEST_CASE("SIP Parser: conditionals", "[TIP Parser]") {
+TEST_CASE("SIP Parser: LTE conditional positive case", "[SIP Parser]") {
   std::stringstream stream;
   stream << R"(
       short() {
@@ -13,8 +13,9 @@ TEST_CASE("SIP Parser: conditionals", "[TIP Parser]") {
         iter = 0;
         limit = 50;
         answer = 0;
-        while ( i <= 50) {
+        while ( iter <= 50) {
           answer = answer + iter;
+          iter = iter + 1;
         }
         return answer;
       }
@@ -22,3 +23,25 @@ TEST_CASE("SIP Parser: conditionals", "[TIP Parser]") {
 
   REQUIRE(ParserHelper::is_parsable(stream));
 }
+
+TEST_CASE("SIP Parser: LTE conditional negative case", "[SIP Parser]") {
+  std::stringstream stream;
+  stream << R"(
+      short() {
+        var x, y, z;
+        y = -1;
+        z = 1;
+        x = 50;
+        if (<50) {
+          z = y;
+        }
+        else {
+          y = z;
+        }
+        return y;
+      }
+    )";
+
+  REQUIRE_FALSE(ParserHelper::is_parsable(stream));
+}
+
