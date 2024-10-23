85b03cd655ba0a9c7c9ed7c2a626a3c0c5cff64b
Devang Ray <gbc2rx@virginia.edu>
2024-10-23 13:12:05 -0400

    wrote test for ASTForRange node

---
+++ src/frontend/ast/treetypes/ASTForRangeStmt.cpp
@@ -0,0 +1,47 @@
+//
+// Created by gbc2rx on 10/14/24.
+// Based on ASTIfStmt.cpp
+
+#include "ASTForRangeStmt.h"
+#include "ASTVisitor.h"
+
+void ASTForRangeStmt::accept(ASTVisitor *visitor) {
+  if (visitor->visit(this)) {
+    getInitializer()->accept(visitor);
+    getRangeStart()->accept(visitor);
+    getRangeEnd()->accept(visitor);
+    if (getStep() != nullptr) {
+      getStep()->accept(visitor);
+    }
+    getBody()->accept(visitor);
+  }
+  visitor->endVisit(this);
+}
+
+std::ostream &ASTForRangeStmt::print(std::ostream &out) const {
+    out << "for (" << *getInitializer() << " : " << *getRangeStart() << " .. " << *getRangeEnd();
+    if (getStep() != nullptr) {
+        out << " by " << *getStep();
+    }
+    out << ") " << *getBody();
+
+    return out;
+}
+
+std::vector<std::shared_ptr<ASTNode>> ASTForRangeStmt::getChildren() {
+  std::vector<std::shared_ptr<ASTNode>> children;
+
+  children.push_back(INITIALIZER);
+  children.push_back(RANGESTART);
+  children.push_back(RANGEEND);
+  if (getStep() != nullptr) {
+    children.push_back(STEP);
+  }
+  children.push_back(BODY);
+
+  return children;
+}// LCOV_EXCL_LINE
+
+llvm::Value* ASTForRangeStmt::codegen() {// LCOV_EXCL_LINE
+  return nullptr;// LCOV_EXCL_LINE
+}// LCOV_EXCL_LINE
