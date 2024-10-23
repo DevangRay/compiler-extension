cfdb7f774b4fc2a9e1841b1541f871b03721f095
Devang Ray <gbc2rx@virginia.edu>
2024-10-23 13:10:01 -0400

    merged in files from main

--- test/unit/frontend/PreOrderIteratorTest.cpp
+++ test/unit/frontend/PreOrderIteratorTest.cpp
@@ -42,8 +42,151 @@ TEST_CASE("PreOrderIterator: Test ArrayExpr", "[PreOrderIterator]") {
   for (auto iter = syntaxTree.begin(""); iter != syntaxTree.end(""); ++iter) {
     std::stringstream actual_node;
     actual_node << *iter->getRoot();
-//    std::cout << actual_node.str();
     REQUIRE(expected_node_order.at(i++) == actual_node.str());
+  }
+}
+
+TEST_CASE("PreOrderIterator: Test ForRangeStmt with by child", "[PreOrderIterator]") {
+  std::stringstream stream;
+  stream << R"(
+      short() {
+        var x, answer;
+        x = [1, 0, 4];
+        for (item : 0 .. 10 by 2) answer = answer + item;
+        return answer;
+      }
+    )";
+
+  std::shared_ptr<ASTProgram> ast = std::move(ASTHelper::build_ast(stream));
+
+  std::vector<std::string> expected_node_order = {
+    "49c200d6e706d633c00da64e9db9ecc07431e823dfedb6330a6fbf66ae4dc9a0",
+    "short() {...}",
+    "short",
+    "var x, answer;",
+    "x",
+    "answer",
+    "x = [ 1, 0, 4 ];",
+    "x",
+    "[ 1, 0, 4 ]",
+    "1",
+    "0",
+    "4",
+    "for (item : 0 .. 10 by 2) answer = (answer+item);",
+    "item",
+    "0",
+    "10",
+    "2",
+    "answer = (answer+item);",
+    "answer",
+    "(answer+item)",
+    "answer",
+    "item",
+    "return answer;",
+    "answer",
+};
+
+  SyntaxTree syntaxTree(ast);
+  int i = 0;
+  for (auto iter = syntaxTree.begin(""); iter != syntaxTree.end(""); ++iter) {
+    std::stringstream actual_node;
+    actual_node << *iter->getRoot();
+//        std::cout << actual_node.str() << "\n";
+    REQUIRE(expected_node_order.at(i++) == actual_node.str());
+  }
+}
+
+TEST_CASE("PreOrderIterator: Test ForRangeStmt with no by child", "[PreOrderIterator]") {
+  std::stringstream stream;
+  stream << R"(
+      short() {
+        var x, answer;
+        x = [1, 0, 4];
+        for (item : 0 .. 5) answer = answer + item;
+        return answer;
+      }
+    )";
+
+  std::shared_ptr<ASTProgram> ast = std::move(ASTHelper::build_ast(stream));
+
+  std::vector<std::string> expected_node_order = {
+    "def53103c2ee17eee8f0d54c6dfa84070d9403c298075427c4f9584b108fc22b",
+    "short() {...}",
+    "short",
+    "var x, answer;",
+    "x",
+    "answer",
+    "x = [ 1, 0, 4 ];",
+    "x",
+    "[ 1, 0, 4 ]",
+    "1",
+    "0",
+    "4",
+    "for (item : 0 .. 5) answer = (answer+item);",
+    "item",
+    "0",
+    "5",
+    "answer = (answer+item);",
+    "answer",
+    "(answer+item)",
+    "answer",
+    "item",
+    "return answer;",
+    "answer",
+};
+
+  SyntaxTree syntaxTree(ast);
+  int i = 0;
+  for (auto iter = syntaxTree.begin(""); iter != syntaxTree.end(""); ++iter) {
+    std::stringstream actual_node;
+    actual_node << *iter->getRoot();
+    //        std::cout << actual_node.str() << "\n";
+    REQUIRE(expected_node_order.at(i++) == actual_node.str());
+  }
+}
+
+TEST_CASE("PreOrderIterator: Test NegExpr", "[PreOrderIterator]") {
+  std::stringstream stream;
+  stream << R"(
+      short() {
+        var x, answer;
+        x = input;
+        x = -x;
+        return answer;
+      }
+    )";
+
+  std::shared_ptr<ASTProgram> ast = std::move(ASTHelper::build_ast(stream));
+
+  std::vector<std::string> expected_node_order = {
+    "short() {...}",
+    "short",
+    "var x, answer;",
+    "x",
+    "answer",
+    "x = input;",
+    "x",
+    "input",
+    "x = - x;",
+    "x",
+    "- x",
+    "x",
+    "return answer;",
+    "answer",
+};
+
+  SyntaxTree syntaxTree(ast);
+  int i = -1;
+  for (auto iter = syntaxTree.begin(""); iter != syntaxTree.end(""); ++iter) {
+    if (i != -1) {
+      std::stringstream actual_node;
+      actual_node << *iter->getRoot();
+//      std::cout << actual_node.str() << "\n";
+          REQUIRE(expected_node_order.at(i++) == actual_node.str());
+    }
+    else{
+      i++;
+    }
   }
 }
 //End Extension
