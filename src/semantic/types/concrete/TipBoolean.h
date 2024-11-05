2e9fde5d62d89bc4675b8f6ed53eff18be15a473
Saidamir Umarkhodjaev <fyg6db@virginia.edu>
2024-11-05 17:19:30 -0500

    added some stuff

---
+++ src/semantic/types/concrete/TipBoolean.h
@@ -0,0 +1,24 @@
+#pragma once
+
+#include "TipCons.h"
+#include "TipType.h"
+#include <string>
+
+/*!
+ * \class TipInt
+ *
+ * \brief A proper type representing an int
+ */
+class TipBoolean : public TipCons {
+public:
+    TipBoolean();
+
+    bool operator==(const TipType &other) const override;
+    bool operator!=(const TipType &other) const override;
+
+    void accept(TipTypeVisitor *visitor) override;
+
+protected:
+    std::ostream &print(std::ostream &out) const override;
+};
+
